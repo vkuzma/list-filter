90b209e1a2555b01440e26f60b3f811235685a76
vkuzma <kuzma@allink.ch>
2013-04-09 23:57:52 +0200

    working on filtering implemented exact filter

--- src/lists-filtering.js
+++ src/lists-filtering.js
@@ -1,14 +1,36 @@
-Array.prototype.filter_by = function(filters) {
+function filter_list(list, filters) {
+	var filter_actions = {
+		'exact': function(value, filter_value) {
+			if(value == filter_value)
+				return true;
+			else
+				return false;
+		}
+	};
+	var filter_matched = function(item, filter_key, filter_value) {
+		var filter_parts = filter_key.split('__');
+		if(!filter_parts.length)
+			return filters.exact;
+		var filter_action = filter_actions[filter_parts[1]];
+		if(filter_parts[0])
+			return filter_action(item[filter_parts[0]], filter_value);
+		else
+			return false;
+	};
+
 	var new_list = [];
 	var match = true;
-	for(var i = 0; i < this.length; i++) {
-		for(var filter_key in filters) {
-			if(this[i][filter_key] != filters[filter_key])
-				match = false
-			if(match) {
-				new_list.push(this[i]);
+	for(var i = 0; i < list.length; i++) {
+		for(var item_key in list[i]) {
+			for(var filter_key in filters) {
+				if(!filter_matched(list[i], filter_key, filters[filter_key]))
+					match = false;
+				}
+				if(match) {
+					new_list.push(list[i]);
 			}
 		}
 	}
 	return new_list;
 }
+
